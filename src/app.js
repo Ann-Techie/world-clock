c97a0c2927d8f31321c35b6e6a258d2e0afb130d
Ann Kamau <amuringok@gmail.com>
2025-03-18 00:37:22 +0300

    added a different options of cities

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
 function updateTime() {
   let nairobiCity = document.querySelector("#nairobi");
+  if (nairobiCity) {
+    let nairobiDate = nairobiCity.querySelector(".date");
+    nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
 
-  let nairobiDate = nairobiCity.querySelector(".date");
-  nairobiDate.innerHTML = moment().format("MMMM Do YYYY");
+    let nairobiTime = nairobiCity.querySelector(".time");
+    nairobiTime.innerHTML = moment()
+      .tz("Africa/Nairobi")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
 
-  let nairobiTime = nairobiCity.querySelector(".time");
-  nairobiTime.innerHTML = moment()
-    .tz("Africa/Nairobi")
-    .format("h:mm:ss [<small>]A[</small>]");
+  let djiboutiCity = document.querySelector("#djibouti");
+  if (djiboutiCity) {
+    let djiboutiDate = djiboutiCity.querySelector(".date");
+    djiboutiDate.innerHTML = moment().format("MMMM Do YYYY");
 
-  let tanzaniaCity = document.querySelector("#tanzania");
+    let djiboutiTime = djiboutiCity.querySelector(".time");
+    djiboutiTime.innerHTML = moment()
+      .tz("Africa/Djibouti")
+      .format("h:mm:ss [<small>]A[</small>]");
+  }
+}
 
-  let tzDate = tanzaniaCity.querySelector(".date");
-  tzDate.innerHTML = moment().format("MMMM Do YYYY");
+function updateTimeZone(event) {
+  let city = event.target.value;
+  let cityTimeZone = moment().tz(city);
+  let cityName = city.replace("-", " ").split("/")[1];
+  let citiesHtml = document.querySelector("#cities");
+  citiesHtml.innerHTML = `    <div class="city">
+          <div>
+  <h2>${cityName}</h2>
 
-  let tzTime = tanzaniaCity.querySelector(".time");
-  tzTime.innerHTML = moment()
-    .tz("Africa/Tanzania")
-    .format("h:mm:ss [<small>]A[</small>]");
+            <div class="date">${cityTimeZone.format("dddd MMMM Do, YYYY")}</div>
+          </div>
+          <div class="time">${cityTimeZone.format(
+            "h:mm:ss"
+          )} <small>${cityTimeZone.format("A")}</small></div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1);
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateTimeZone);
